b798375d7b8531a798c27c2e954a08c8a2225a50
sergdort <sergdort@gmail.com>
2019-06-05 17:55:04 -0700

    Initial Commit

---
+++ CombineFeedback/CombineFeedback.h
@@ -0,0 +1,19 @@
+//
+//  CombineFeedback.h
+//  CombineFeedback
+//
+//  Created by Sergey Shulga on 05/06/2019.
+//  Copyright © 2019 babylonhealth. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CombineFeedback.
+FOUNDATION_EXPORT double CombineFeedbackVersionNumber;
+
+//! Project version string for CombineFeedback.
+FOUNDATION_EXPORT const unsigned char CombineFeedbackVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CombineFeedback/PublicHeader.h>
+
+
