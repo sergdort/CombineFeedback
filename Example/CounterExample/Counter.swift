843f32d2418f9f802e9512fa8a4453b7453dbc77
sergdort <sergdort@gmail.com>
2021-06-12 20:35:12 +0100

    Fix crash in the example

--- Example/CounterExample/Counter.swift
+++ Example/CounterExample/Counter.swift
@@ -4,51 +4,53 @@ import CombineFeedbackUI
 import SwiftUI
 
 extension Counter {
-    final class ViewModel: Store<Counter.State, Counter.Event> {
-        init() {
-            super.init(
-                initial: State(),
-                feedbacks: [],
-                reducer: Counter.reducer()
-            )
-        }
+  final class ViewModel: Store<Counter.State, Counter.Event> {
+    init() {
+      super.init(
+        initial: State(),
+        feedbacks: [],
+        reducer: Counter.reducer()
+      )
     }
+  }
 }
 
 struct CounterView: View {
-    typealias State = Counter.State
-    typealias Event = Counter.Event
-    
-    @ObservedObject
-    var context: Context<State, Event>
-    
-    init(context: Context<State, Event>) {
-        self.context = context
-        logInit(of: self)
-    }
+  typealias State = Counter.State
+  typealias Event = Counter.Event
 
-    var body: some View {
-        logBody(of: self)
-        return Form {
-            Button(action: {
-                self.context.send(event: .decrement)
-            }) {
-                return Text("-").font(.largeTitle)
-            }
-            Button(action: {
-                self.context.send(event: .increment)
-            }) {
-                Text("+").font(.largeTitle)
-            }
-            ForEach((0..<context.count).reversed(), id: \.self) { (item) in
-                Text("\(item)")
-            }
+  @ObservedObject
+  var context: Context<State, Event>
+
+  init(context: Context<State, Event>) {
+    self.context = context
+    logInit(of: self)
+  }
+
+  var body: some View {
+    logBody(of: self)
+    return Form {
+      Button(action: {
+        self.context.send(event: .decrement)
+      }) {
+        Text("-").font(.largeTitle)
+      }
+      Button(action: {
+        self.context.send(event: .increment)
+      }) {
+        Text("+").font(.largeTitle)
+      }
+      if context.count >= 0 {
+        ForEach((0 ..< context.count).reversed(), id: \.self) { item in
+          Text("\(item)")
         }
+      }
     }
+  }
 }
 
 extension View {
-    func eraseToAnyView() -> AnyView {
-        return AnyView(self)
-    }
+  func eraseToAnyView() -> AnyView {
+    return AnyView(self)
+  }
 }
